d4b6a5886ac8324a8c6591bbe4306d61d3c6694f
Justin Slattery <justin@zeroarc.com>
2017-03-09 13:35:10 -0800

    Change config structure, add flow types, update npm modules

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,82 +1,106 @@
-/* tabletable - Copyright 2015 Zeroarc Software, LLC
- *
- * Top level container component
- */
-
+//@flow
+// tabletable - Copyright 2017 Zeroarc Software, LLC
 'use strict';
 
-// External
-let React = require('react');
-let ReactShallowCompare = require('react-addons-shallow-compare');
-let Immutable = require('immutable');
-let ClassNames = require('classnames');
-let Autobind = require('autobind-decorator');
+import React from 'react';
+import ReactShallowCompare from 'react-addons-shallow-compare';
+import * as Immutable from 'immutable';
+import ClassNames from 'classnames';
+import Autobind from 'autobind-decorator';
 
 // Local
 import Pager from './Pager';
+import type { Data, Columns, Row, Context } from './types';
+
+type Props = {
+  data: Data,
+  columns: Columns,
+  rowsPerPage: number,
+  pagerSize: number,
+  showPager: bool,
+  showFilter: bool,
+  // Optional
+  rowContext?: (Row,number) => any,
+  rowCssClass?: (Row,number,Context) => string,
+  pager?: any, // TODO WTH is the type for this
+  onFilterAction?: (string) => void,
+  filterValue?: string,
+}
+
+type State = {
+  currentPage: number,
+}
 
 
 @Autobind
 export default class TabletableContainer extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+  props: Props;
+  state: State;
+
+  static defaultProps: {
+    rowsPerPage: number,
+    pagerSize: number,
+    showPager: bool,
+    showFilter: bool,
+  }
+
+  constructor(props: Props) {
+    super(props);
+
+    if (!Immutable.Iterable.isIterable(props.data)) {
+      return new Error('Invalid prop data supplied to TableTable. Expected Immutable iterable.');
+    }
 
     this.state = {
       currentPage: 1,
     };
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps: Props, nextState: State) {
     return ReactShallowCompare(this, nextProps, nextState);
   }
 
   render() {
     let headerComponents = [];
 
-    Object.keys(this.props.columns).forEach(k => {
+    this.props.columns.forEach((col,i) => {
       // If visible is false, hide the column. If visible is not defined, default to showing column
-      if (typeof this.props.columns[k].visible === 'undefined' || this.props.columns[k].visible) {
+      if (typeof col.visible === 'undefined' || col.visible) {
         headerComponents.push(
-          <th key={`th-${k}`} className={this.props.columns[k].headerCssClass}>{this.props.columns[k].display || k}</th>
+          <th key={`th-${i}`} className={col.headerCssClass}>{col.display || ''}</th>
         );
       }
     });
 
-    let skipRows = this.props.rowsPerPage * (this.state.currentPage - 1);
-    let takeRows = Math.min(this.props.rowsPerPage, this.props.data.count() - skipRows);
+    const skipRows: number = this.props.rowsPerPage * (this.state.currentPage - 1);
+    const takeRows: number = Math.min(this.props.rowsPerPage, this.props.data.count() - skipRows);
 
-    let rows = this.props.data.skip(skipRows).take(takeRows).map((row,index) => {
+    const rows = this.props.data.skip(skipRows).take(takeRows).map((row,index) => {
       // Create row context if required. Make it an immutable so nobody tries to abuse it by shoving stuff into it
       // during a column step. We will re-project from the Immutable each time it is used
       const context = Immutable.fromJS(this.props.rowContext ? this.props.rowContext(row,index) : {});
 
       // Assign any row classes
-      let rowCssClass = '';
+      let rowCssClass = this.props.rowCssClass;
       if (typeof this.props.rowCssClass === 'function') {
-        rowCssClass = this.props.rowCssClass(row,index,context.toObject());
+        rowCssClass = this.props.rowCssClass(row,index,context && context.toObject());
         if (typeof rowCssClass !== 'string') console.error('rowCssClass function must return a string value. Was ' + typeof rowCssClass);
-      }
-      else {
-        rowCssClass = this.props.rowCssClass;
       }
 
       // Build out components for the row
       let rowComponents = [];
-      Object.keys(this.props.columns).forEach(k => {
+      this.props.columns.forEach((col,i) => {
         // If visible is false, hide the column. If visible is not defined, default to showing column
-        if (typeof this.props.columns[k].visible === 'undefined' || this.props.columns[k].visible) {
+        if (typeof col.visible === 'undefined' || col.visible) {
           // elementCssClass can either be a string or a function that returns a string
-          let elementCssClass = '';
-          if (typeof this.props.columns[k].elementCssClass === 'function') {
-            elementCssClass = this.props.columns[k].elementCssClass(row,index,context.toObject());
+          let elementCssClass = col.elementCssClass;
+          if (typeof col.elementCssClass === 'function') {
+            elementCssClass = col.elementCssClass(row,index,context && context.toObject());
             if (typeof elementCssClass !== 'string') console.error('elementCssClass function must return a string value. Was ' + typeof elementCssClass);
-          }
-          else {
-            elementCssClass = this.props.columns[k].elementCssClass;
           }
 
           rowComponents.push(
-            <td key={`${index}-${k}`} className={elementCssClass}>{this.props.columns[k].data(row,index,context.toObject())}</td>
+            <td key={`${index}-${i}`} className={elementCssClass}>{col.data(row,index,context && context.toObject())}</td>
           );
         }
       });
@@ -89,9 +113,9 @@ export default class TabletableContainer extends React.Component {
     });
 
     // Setup pager
-    let totalPages = Math.ceil(this.props.data.count() / this.props.rowsPerPage);
+    const totalPages: number = Math.ceil(this.props.data.count() / this.props.rowsPerPage);
 
-    let pager = '';
+    let pager: null | React$Element<{currentPage: number, displayPages: number, maxPage: number, onPageChange: (pageNumber: number) => void}> = null;
 
     if (this.props.showPager) {
       // Check for custom pager and use it
@@ -113,60 +137,62 @@ export default class TabletableContainer extends React.Component {
     }
 
     // Hide filter unless we have a search handler and showFilter is true
-    let filterClasses = ClassNames({
+    const filterClasses = ClassNames({
       hidden: !this.props.onFilterAction || !this.props.showFilter
     });
 
-    let clearClasses = ClassNames('btn', 'btn-white', 'btn-xs', {
+    const clearClasses: string = ClassNames('btn', 'btn-white', 'btn-xs', {
       hidden: !this.props.filterValue || this.props.filterValue.length === 0
     });
 
-    let filterControl = this.props.showFilter
+    const filterControl = this.props.showFilter
       ? <div className={filterClasses}>
         <div className='input-group col-xs-4 col-xs-offset-8'>
-          <button className={clearClasses} style={{position: 'absolute', right: '6px', top: '6px', zIndex: 3}} onClick={this.handleClearFilterClick}><i className='fa fa-times'></i> Clear</button>
+          <button className={clearClasses} style={{position: 'absolute', right: '6px', top: '6px', zIndex: 3}} onClick={this.handleClearFilterClick}>
+            <i className='fa fa-times'></i> Clear
+          </button>
           <input type='text' className='form-control' placeholder='Type to filter' value={this.props.filterValue} onChange={this.handleFilterChange} />
         </div>
       </div>
       : '';
 
-      return (
-        <div className='tabletable'>
-          {filterControl}
-          {pager}
-          <table className='table table-striped table-bordered table-hover'>
-            <thead>
-              <tr>
-                {headerComponents}
-              </tr>
-            </thead>
-            <tbody>
-              {rows.toList()}
-            </tbody>
-          </table>
-          {pager}
-        </div>
-      );
+    return (
+      <div className='tabletable'>
+        {filterControl}
+        {pager}
+        <table className='table table-striped table-bordered table-hover'>
+          <thead>
+            <tr>
+              {headerComponents}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.toList()}
+          </tbody>
+        </table>
+        {pager}
+      </div>
+    );
   }
 
   //
   // Custom methods
   //
 
-  handlePageChange(pageNumber) {
+  handlePageChange(pageNumber: number) {
     this.setState({currentPage: pageNumber});
   }
 
   // Update local state and call external onFilterAction if defined
-  handleFilterChange(e) {
+  handleFilterChange(e: SyntheticInputEvent) {
     e.stopPropagation();
     // Reset to first page in case we end up with less pages than current page number
     this.setState({currentPage: 1});
     this.props.onFilterAction && this.props.onFilterAction(e.target.value);
   }
 
-  handleClearFilterClick(e) {
-    e.stopPropagation();
+  handleClearFilterClick(e: SyntheticInputEvent) {
+    e.preventDefault();
     // Reset to first page to re-orient user
     this.setState({currentPage: 1});
     this.props.onFilterAction && this.props.onFilterAction('');
@@ -178,26 +204,4 @@ TabletableContainer.defaultProps = {
   pagerSize: 10,
   showPager: true,
   showFilter: false,
-};
-
-TabletableContainer.propTypes = {
-  data: (props, propName, componentName) => {
-    if (!Immutable.Iterable.isIterable(props[propName])) {
-      return new Error(
-          'Invalid prop `' + propName + '` supplied to' +
-          ' `' + componentName + '`. Expected Immutable iterable.'
-        );
-    }
-  },
-  columns: React.PropTypes.object.isRequired,
-  rowsPerPage: React.PropTypes.number.isRequired,
-  pagerSize: React.PropTypes.number.isRequired,
-  showPager: React.PropTypes.bool.isRequired,
-  showFilter: React.PropTypes.bool.isRequired,
-  // Optional
-  rowContext: React.PropTypes.func,
-  rowCssClass: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.func]),
-  pager: React.PropTypes.func,
-  onFilterAction: React.PropTypes.func,
-  filterValue: React.PropTypes.string,
 };
---
+++ src/types.js
@@ -0,0 +1,23 @@
+// @flow
+// Copyright 2017 Zeroarc Software, LLC
+
+import * as Immutable from 'immutable';
+
+
+// Data can be any shape as long as it is Iterable
+export type Data = Immutable.Iterable<number,any>;
+
+// Row can be anything
+export type Row = any;
+
+export type Context = Immutable.Map<any,any> | Immutable.List<any>;
+
+// Columns have specific properties
+type Column = {
+  display: string,
+  headerCssClass: string,
+  elementCssClass: (Row,number,Context) => string,
+  data: (Row,number) => React$Element<*>
+};
+export type Columns = Array<Column>;
+
