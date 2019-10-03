1c2d0846838fba99c88162db619f3d00b5011b43
Justin Slattery <justin@zeroarc.com>
2019-10-03 12:24:41 -0700

    Handle paging correctly when clearing filter.

--- src/Container.jsx
+++ src/Container.jsx
@@ -248,15 +248,19 @@ export default class TabletableContainer extends React.PureComponent<Props, Stat
 
   handleClearFilterClick(e: SyntheticInputEvent<*>) {
     e.preventDefault();
+    
     // Reset to first page to re-orient user
-    this.setState({currentPage: 1, filterValue: ''});
-    this.props.onClear && this.props.onClear();
+    if (this.state.filterValue) {
+      this.setState({filterValue: ''});
+      this.handlePageChange(1);
+      this.props.onClear && this.props.onClear();
+    }
   }
 
   handleKeyPress(e: SyntheticKeyboardEvent<*>) {
     if (e.key === 'Enter') {
       e.preventDefault();
-      this.setState({currentPage: 1});
+      this.handlePageChange(1);
       this.props.onSearch && this.props.onSearch(this.state.filterValue);
     }
   }
