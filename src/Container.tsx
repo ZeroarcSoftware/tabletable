12826092824be2d6bf482c4f50e6b47438aa464c
Hans Gutknecht <hansdotcom@gmail.com>
2020-04-03 14:09:42 -0400

    Stashing state to debug link

--- src/Container.tsx
+++ src/Container.tsx
@@ -26,8 +26,8 @@ type Props = {
   onPageChange?: (page: number) => void,
   rowContext?: (Row, number) => any,
   rowCssClass?: (Row, number, Context) => string,
-  showSpinner?: bool,
-  spinner?: *,
+  showSpinner?: boolean,
+  spinner?: any,
   totalRows?: number,
 }
 
