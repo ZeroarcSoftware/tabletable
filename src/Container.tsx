251c6052cc292bd9711416d4dea67449a3318123
Justin Slattery <justin@zeroarc.com>
2021-11-11 10:55:42 -0800

    Make error message float, update npm packages.

--- src/Container.tsx
+++ src/Container.tsx
@@ -321,7 +321,7 @@ const TabletableContainer: FunctionComponent<Props> = ({
       ? (
         <tr key={`td-${index}-error`}>
           <td colSpan={columns.length} className='text-danger' style={{ height: '40px' }}>
-            <div style={{ position: 'absolute', left: '20px' }} >
+            <div style={{ position: 'fixed' }} >
               <FontAwesomeIcon icon={['far', 'exclamation-triangle']} fixedWidth /> Error: {error.errorMessage}
             </div>
           </td>
